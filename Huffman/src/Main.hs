b9b395f175142b6285151c504913d7640ac54c52
iostreamer-X <iostreamer007@gmail.com>
2017-04-13 15:04:38 +0530

    Added a few comments emphasizing what I learnt.

--- Huffman/src/Main.hs
+++ Huffman/src/Main.hs
@@ -5,6 +5,7 @@ import Coding
 import MakeCode
 import CodeTable
 
+-- This little supervised project taught me about code structure, patterns and modules.
 
 testString = "battat"
 
@@ -14,4 +15,5 @@ huffTable = codeTable huffTree
 codedMessage = codeMessage huffTable testString
 decodedMessage = decodeMessage huffTree codedMessage
 
+-- decodedMessage must be "battat" if I managed to code everything properly
 main = print decodedMessage
